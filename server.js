082cf0604ea220fbe676b4be565b31bb7e115de3
kKrishGupta <kg20060220@gmail.com>
2026-01-20 00:14:24 +0530

    structure of project is created

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const rootDir = require('./utils/pathUtils');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(rootDir, 'public')));
@@ -9,7 +10,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
-res.render('index');
+  fs.readdir(`./files`,function(err, files){
+    console.log(files);
+    res.render('index', { files: files });
+  });
+});
+
+app.post('/create', (req, res) => {
+  console.log(req.body);
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+      return res.status(500).send('Internal Server Error');
+    }res.redirect('/');
+  });
 });
 
 app.listen(3000, () => {
